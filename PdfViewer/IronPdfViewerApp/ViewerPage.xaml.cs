9909b8930e6b27469988320c0743db5cc4f508c4
JD Pierce <brkfstmnchr@gmail.com>
2023-09-11 17:58:43 +0700

    add webview example
    add webview example

--- PdfViewer/IronPdfViewerApp/ViewerPage.xaml.cs
+++ PdfViewer/IronPdfViewerApp/ViewerPage.xaml.cs
@@ -12,7 +12,21 @@ public partial class ViewerPage : ContentPage
     private async void Button_OnClicked(object sender, EventArgs e)
     {
         ChromePdfRenderer renderer = new ChromePdfRenderer();
-        var doc = renderer.RenderHtmlAsPdf(htmlContent.Text);
+        var doc = renderer.RenderUrlAsPdf(htmlContent.Text);
         this.pdfView.Source = IronPdfViewSource.FromBytes(doc.BinaryData);
+    }
+
+    private async void ViewerPage_OnLoaded(object sender, EventArgs e)
+    {
+        htmlContent.Text = "https://ironpdf.com/";
+    }
+
+    private async void HtmlContent_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        this.webView.Source = htmlContent.Text;
+    }
+
+    private void WebView_OnNavigated(object sender, WebNavigatedEventArgs e)
+    {
     }
 }
